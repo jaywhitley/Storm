1eed1177438999587c0de461f7ee029d915a514a
jay whitley jr. <jay@whisper.sh>
2015-08-24 16:30:09 -0700

    Create Player.swift

---
+++ Player.swift
@@ -0,0 +1,23 @@
+//
+
+import Foundation
+
+class Player : Team {
+
+    var playerId = ""
+    var name = ""
+    var age:Int = 0
+    var teams:Int = 0
+    var about = ""
+    var local = ""
+    var profileImage:PFFile?
+    var extraImage1:PFFile?
+    var extraImage2:PFFile?
+    var extraImage3:PFFile?
+    var onTeam = false
+    var isFemale = false
+
+
+    
+
+}
